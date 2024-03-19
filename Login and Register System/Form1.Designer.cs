e75a6fc4e5fd9b9fec0e47a54cba16b511da6d0a
bernadettmarsalik <bernadett.marsalik@gmail.com>
2024-03-19 17:00:43 +0100

    Add project files.

---
+++ Login and Register System/Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace Login_and_Register_System
+{
+    partial class frmRegister
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // frmRegister
+            // 
+            AutoScaleDimensions = new SizeF(10F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(285, 544);
+            Font = new Font("Nirmala UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ForeColor = Color.FromArgb(164, 165, 169);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "frmRegister";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Login and Register System/Form1.cs
@@ -0,0 +1,10 @@
+namespace Login_and_Register_System
+{
+    public partial class frmRegister : Form
+    {
+        public frmRegister()
+        {
+            InitializeComponent();
+        }
+    }
+}
